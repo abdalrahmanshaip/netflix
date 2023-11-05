9588c24ba09e6a93585cfcb4c98a1bac7f350aef
abdalrhman <abdalrahmanshaip@gmail.com>
2023-11-05 20:35:49 +0200

    Edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel='icon' href='./Netflix-Symbol.png'/>
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
